0fd33cf01714d177ace3146e2ce513b5448de15a
pavithra30 <EVOLVUS\kamalaranip@EPCBLR-044.evolvus.com>
2018-06-25 16:36:23 +0530

    changed findAll to filter by entityName,tenantId and accesslevel

--- db/entity.js
+++ db/entity.js
@@ -1,54 +1,64 @@
 const debug = require("debug")("evolvus-branch:db:branch");
 const mongoose = require("mongoose");
 const ObjectId = require('mongodb')
-    .ObjectID;
+  .ObjectID;
 
 const entitySchema = require("./entitySchema");
 
 // Creates a branchCollection collection in the database
 var entityCollection = mongoose.model("entityCollection", entitySchema);
 module.exports.branchSchema = {
-    entitySchema
+  entitySchema
 };
 // Saves the branchCollection object to the database and returns a Promise
 // The assumption here is that the Object is valid
 module.exports.save = (object) => {
-    return new Promise((resolve, reject) => {
-        try {
-            // any exception during construction will go to catch
-            let branch = new branchCollection(object);
-            // on resolve we need to resolve the this method
-            // on reject or exception we reject it,
-            // this is because the record either saves or it doesnt
-            // in any case it does not save, is a reject
-            branch.save()
-                .then((data) => {
-                    debug("saved successfully", data._id);
-                    resolve(data);
-                }, (err) => {
-                    debug(`rejected save.. ${err}`);
-                    reject(err);
-                })
-                .catch((e) => {
-                    debug(`exception on save: ${e}`);
-                    reject(e);
-                });
-        } catch (e) {
-            debug(`caught exception: ${e}`);
-            reject(e);
-        }
-    });
+  return new Promise((resolve, reject) => {
+    try {
+      // any exception during construction will go to catch
+      let branch = new entityCollection(object);
+      // on resolve we need to resolve the this method
+      // on reject or exception we reject it,
+      // this is because the record either saves or it doesnt
+      // in any case it does not save, is a reject
+      branch.save()
+        .then((data) => {
+          debug("saved successfully", data._id);
+          resolve(data);
+        }, (err) => {
+          debug(`rejected save.. ${err}`);
+          reject(err);
+        })
+        .catch((e) => {
+          debug(`exception on save: ${e}`);
+          reject(e);
+        });
+    } catch (e) {
+      debug(`caught exception: ${e}`);
+      reject(e);
+    }
+  });
 };
 
 
-// Returns a limited set if all the branch(s) with a Promise
+
+// Returns a limited set if all the role(s) with a Promise
 // if the collectiom has no records it Returns
 // a promise with a result of  empty object i.e. {}
-module.exports.findAll = (limit) => {
-    if (limit < 1) {
-        return branchCollection.find({});
-    }
-    return branchCollection.find({}).limit(limit);
+module.exports.findAll = (tenantId, name, accessLevel, limit, orderBy) => {
+  let query = {
+    tenantId: tenantId,
+    accessLevel: {
+      $gte: accessLevel
+    },
+    name: name
+  };
+
+  if (limit < 1) {
+    return entityCollection.find(query).sort(orderBy);
+  } else {
+    return entityCollection.find(query).limit(limit).sort(orderBy);
+  }
 };
 
 // Finds the branch which matches the value parameter from branch collection
@@ -56,27 +66,27 @@ module.exports.findAll = (limit) => {
 // null, undefined should be rejected with Invalid Argument Error
 // Should return a Promise
 module.exports.findOne = (attribute, value) => {
-    return new Promise((resolve, reject) => {
-        try {
-            var query = {};
-            query[attribute] = value;
-            branchCollection.findOne(query)
-                .then((data) => {
-                    debug(`branch found ${data}`);
-                    resolve(data);
-                }, (err) => {
-                    debug(`rejected find.. ${err}`);
-                    reject(err);
-                })
-                .catch((e) => {
-                    debug(`exception on find: ${e}`);
-                    reject(e);
-                });
-        } catch (e) {
-            debug(`caught exception: ${e}`);
-            reject(e);
-        }
-    });
+  return new Promise((resolve, reject) => {
+    try {
+      var query = {};
+      query[attribute] = value;
+      entityCollection.findOne(query)
+        .then((data) => {
+          debug(`branch found ${data}`);
+          resolve(data);
+        }, (err) => {
+          debug(`rejected find.. ${err}`);
+          reject(err);
+        })
+        .catch((e) => {
+          debug(`exception on find: ${e}`);
+          reject(e);
+        });
+    } catch (e) {
+      debug(`caught exception: ${e}`);
+      reject(e);
+    }
+  });
 };
 
 // Finds all the branchs which matches the value parameter from branch collection
@@ -84,27 +94,27 @@ module.exports.findOne = (attribute, value) => {
 // null, undefined should be rejected with Invalid Argument Error
 // Should return a Promise
 module.exports.findMany = (attribute, value) => {
-    return new Promise((resolve, reject) => {
-        try {
-            var query = {};
-            query[attribute] = value;
-            branchCollection.find(query)
-                .then((data) => {
-                    debug(`branch found ${data}`);
-                    resolve(data);
-                }, (err) => {
-                    debug(`rejected find.. ${err}`);
-                    reject(err);
-                })
-                .catch((e) => {
-                    debug(`exception on find: ${e}`);
-                    reject(e);
-                });
-        } catch (e) {
-            debug(`caught exception: ${e}`);
-            reject(e);
-        }
-    });
+  return new Promise((resolve, reject) => {
+    try {
+      var query = {};
+      query[attribute] = value;
+      entityCollection.find(query)
+        .then((data) => {
+          debug(`branch found ${data}`);
+          resolve(data);
+        }, (err) => {
+          debug(`rejected find.. ${err}`);
+          reject(err);
+        })
+        .catch((e) => {
+          debug(`exception on find: ${e}`);
+          reject(e);
+        });
+    } catch (e) {
+      debug(`caught exception: ${e}`);
+      reject(e);
+    }
+  });
 };
 
 // Finds the branch for the id parameter from the branch collection
@@ -113,51 +123,51 @@ module.exports.findMany = (attribute, value) => {
 // All returns are wrapped in a Promise
 //
 module.exports.findById = (id) => {
-    return new Promise((resolve, reject) => {
-        try {
-            branchCollection.findById({
-                    _id: new ObjectId(id)
-                })
-                .then((res) => {
-                    debug("findById successfull: ", res);
-                    resolve(res);
-                }, (err) => {
-                    debug(`rejected finding branchCollection.. ${err}`);
-                    reject(err);
-                })
-                .catch((e) => {
-                    debug(`exception on finding branch: ${e}`);
-                    reject(e);
-                });
-        } catch (e) {
-            debug(`caught exception: ${e}`);
-            reject(e);
-        }
-    });
+  return new Promise((resolve, reject) => {
+    try {
+      entityCollection.findById({
+          _id: new ObjectId(id)
+        })
+        .then((res) => {
+          debug("findById successfull: ", res);
+          resolve(res);
+        }, (err) => {
+          debug(`rejected finding branchCollection.. ${err}`);
+          reject(err);
+        })
+        .catch((e) => {
+          debug(`exception on finding branch: ${e}`);
+          reject(e);
+        });
+    } catch (e) {
+      debug(`caught exception: ${e}`);
+      reject(e);
+    }
+  });
 };
 
 // Filters entity collection by entityDetails
 // Returns a promise
 
-module.exports.filterByBranchDetails = (filterQuery) => {
-    return new Promise((resolve, reject) => {
-        try {
-            branchCollection.find(filterQuery).then((docs) => {
-                debug(`Documents filterd by ${filterQuery} are ${docs}`);
-                resolve(docs);
-            }).catch((e) => {
-                debug(`Failed to filter due to ${e}`);
-                reject(e);
-            });
-        } catch (e) {
-            debug(`Caught Exception ${e}`);
-            reject(e);
-        }
-    });
+module.exports.filterByEntityDetails = (filterQuery) => {
+  return new Promise((resolve, reject) => {
+    try {
+      entityCollection.find(filterQuery).then((docs) => {
+        debug(`Documents filterd by ${filterQuery} are ${docs}`);
+        resolve(docs);
+      }).catch((e) => {
+        debug(`Failed to filter due to ${e}`);
+        reject(e);
+      });
+    } catch (e) {
+      debug(`Caught Exception ${e}`);
+      reject(e);
+    }
+  });
 };
 
 // Deletes all the entries of the collection.
 // To be used by test only
 module.exports.deleteAll = () => {
-    return branchCollection.remove({});
+  return entityCollection.remove({});
 };
